8bb4792955a48b3068f4ef3b44c1f807a253f295
jimboed <polkablister@gmail.com>
2018-08-27 21:11:40 -0500

    z

--- A3.py
+++ A3.py
@@ -1,10 +1,27 @@
-from init import app
+ 
 
-import views, api
+import base64
+import flask
+
+
+
+
+app = flask.Flask(__name__)
+
+
+@app.route('/integrationTest', methods=['GET'])
+def add_message():
+    content = request.get_json(silent=True)
+
+    return content
+
+
+@app.route('/')
+def home():
+    return "hello jim"
 
 
 
 
 if __name__ == '__main__':
     app.run(debug=True)
-
